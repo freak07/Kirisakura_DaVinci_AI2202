f48d077e37db8184802ed3193c0d895c578cf42c
freak07 <michalechner92@googlemail.com>
2022-09-02 06:34:22 +0200

    Revert "sched, cpuset: Fix dl_cpu_busy() panic due to empty cs->cpus_allowed"
    This reverts commit 336626564b58071b8980a4e6a31a8f5d92705d9b.
    
    (cherry picked from commit d5372a621ccb946e28d28c277d5bea1c98ceee2c)
    (cherry picked from commit afc0264c57b990e70e4b29d4c337a0e62d7b4fa9)

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6857,7 +6857,7 @@ int cpuset_cpumask_can_shrink(const struct cpumask *cur,
 }
 
 int task_can_attach(struct task_struct *p,
-		    const struct cpumask *cs_effective_cpus)
+		    const struct cpumask *cs_cpus_allowed)
 {
 	int ret = 0;
 
@@ -6876,11 +6876,9 @@ int task_can_attach(struct task_struct *p,
 	}
 
 	if (dl_task(p) && !cpumask_intersects(task_rq(p)->rd->span,
-					      cs_effective_cpus)) {
-		int cpu = cpumask_any_and(cpu_active_mask, cs_effective_cpus);
+					      cs_cpus_allowed)) {
+		int cpu = cpumask_any_and(cpu_active_mask, cs_cpus_allowed);
 
-		if (unlikely(cpu >= nr_cpu_ids))
-			return -EINVAL;
 		ret = dl_cpu_busy(cpu, p);
 	}
 
